56e73dcb532b4e77a8994bbcb8784d31363d3158
nicooz2 <nicolas.egidio.3a@outlook.com>
2025-04-05 23:27:05 -0300

    Atualização - fábrica de item e colisão item
    Fábrica de item foi adicionada junto ao sprite da garrafa, collider do personagem com o item está no jogo.

---
+++ My project/Assets/Itens/FabricaDeItem.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FabricaDeItem : MonoBehaviour
+{
+    [SerializeField] private Sprite[] ItemSprite;
+
+    [SerializeField] private BoxCollider2D cd;
+    [SerializeField] private GameObject ItemPrefab;
+    [SerializeField] private float cooldown;
+    private float timer;
+
+    private int ItemFeito;
+    private int ItemPontoReferência = 10;
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+
+        if (timer < 0)
+        {
+            timer = cooldown;
+            ItemFeito++;
+
+            if (ItemFeito > ItemPontoReferência && cooldown > .5f)
+            {
+                ItemPontoReferência += 10;
+                cooldown -= .5f;
+
+            }
+
+
+            GameObject newItem = Instantiate(ItemPrefab);
+
+            float randomX = Random.Range(cd.bounds.min.x, cd.bounds.max.x);
+
+            newItem.transform.position = new Vector2(randomX, transform.position.y);
+
+            int RandomIndex = Random.Range(0, ItemSprite.Length);
+            newItem.GetComponent<SpriteRenderer>().sprite = ItemSprite[RandomIndex];
+
+        }
+
+    }
+}
---
+++ My project/Assets/Personagem/ColliderPersonagem.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColliderPersonagem : MonoBehaviour
+{
+    private Rigidbody2D b_Rigidbody2D => GetComponent<Rigidbody2D>();
+
+    // Update is called once per frame
+    void Update()  
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Item")
+        {
+            
+            Destroy(collision.gameObject);
+
+        }
+
+    }
+}
