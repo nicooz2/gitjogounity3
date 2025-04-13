767fe1cc50bc7a88766f3c18fe6df6cfbbf47a0d
nicooz2 <nicolas.egidio.3a@outlook.com>
2025-04-13 01:55:20 -0300

    Update - Interface, barra de vida, nova fase.
    A barra de vida do chão está na interface com o gradiente de cores, arrumar background com a barra. Foi adicionado a fase04.

--- My project/Assets/Chao/VidaChao.cs
+++ My project/Assets/Chao/VidaChao.cs
@@ -1,17 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class VidaChao : MonoBehaviour
 {
     
     public int Vida;
     public int VidaCheia = 10;
+    public Slider BarraDeVida;
+    public Gradient CorGradiente;
+    public Image CorBarra;
 
     // Start is called before the first frame update
     void Start()
     {
         Vida = VidaCheia;
+        BarraDeVida.maxValue = VidaCheia;
+        BarraDeVida.value = Vida;
+
+        CorBarra.color = CorGradiente.Evaluate(1f);
     }
 
     // Update is called once per frame
@@ -23,5 +31,8 @@ public class VidaChao : MonoBehaviour
     public void ChaoComItem (int quantidade)
     {
         Vida -= quantidade;
+        BarraDeVida.value = Vida;
+
+        CorBarra.color = CorGradiente.Evaluate(BarraDeVida.normalizedValue);
     }
 }
